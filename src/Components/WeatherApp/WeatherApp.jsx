2476db9b49aa77a07b53e69732dfcc7c26b8250d
Simeoncoded <simeonolawore07@gmail.com>
2024-05-05 19:32:31 -0400

    Third Commit

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -10,49 +10,70 @@ import wind_icon from "../Assets/wind.png";
 import humidity_icon from "../Assets/humidity.png";
 
 export default function WeatherApp() {
+  let api_key = "f74769a5fb035e1b6dc49df95780a3a8";
 
-let api_key = "f74769a5fb035e1b6dc49df95780a3a8";
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput");
+    if (element[0].value === "") {
+      return 0;
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&appid=${api_key}`;
 
-const search = () => {
+    let response = await fetch(url);
 
-}
+    let data = await response.json();
+
+    const humidity = document.getElementsByClassName("humidity-percent");
+
+    const wind = document.getElementsByClassName("wind-rate");
+
+    const temperature = document.getElementsByClassName("weather-temp");
+
+    const location = document.getElementsByClassName("weather-location");
+
+    humidity[0].innerHTML = data.main.humidity+" %";
+    wind[0].innerHTML = data.wind.speed+ " km/h";
+    temperature[0].innerHTML = data.main.temp+ "°C";
+    location[0].innerHTML = data.name;
+    
 
 
-  return <div className='container'>
-        <div className="top-bar">
-            <input type="text" className="cityInput" placeholder="Search" />
-            <div className="search-icon" onClick={}>
-                <img src={search_icon} alt="" />
-            </div>
+  };
+
+  return (
+    <div className="container">
+      <div className="top-bar">
+        <input type="text" className="cityInput" placeholder="Search" />
+        <div
+          className="search-icon"
+          onClick={() => {
+            search();
+          }}
+        >
+          <img src={search_icon} alt="" />
         </div>
-        <div className="weather-image">
-            <img src={cloud_icon} alt="" />
+      </div>
+      <div className="weather-image">
+        <img src={cloud_icon} alt="" />
+      </div>
+      <div className="weather-temp">24°C</div>
+      <div className="weather-location">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidity_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="text">Humidity</div>
+          </div>
         </div>
-        <div className="weather-temp">
-            24°C
+        <div className="element">
+          <img src={wind_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="wind-rate">18 km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
         </div>
-        <div className="weather-locaton">
-            London
-        </div>
-        <div className="data-container">
-            <div className="element">
-                <img src={humidity_icon} alt="" className="icon"/>
-                <div className="data">
-                    <div className="humidity-percent">
-                        64%
-                    </div>
-                    <div className="text">Humidity</div>
-                </div>
-            </div>
-            <div className="element">
-                <img src={wind_icon} alt="" className="icon"/>
-                <div className="data">
-                    <div className="humidity-percent">
-                       18 km/h
-                    </div>
-                    <div className="text">Wind Speed</div>
-                </div>
-            </div>
-        </div>
-  </div>;
+      </div>
+    </div>
+  );
 }
